0c897e955b93d8b25a54864a4a0d30ad6b46383e
Ubuntu <chemacarceller@yahoo.es>
2025-08-12 20:38:50 +0200

    added comments

--- loadingscene/loadingscreen.gd
+++ loadingscene/loadingscreen.gd
@@ -1,28 +1,36 @@
 # It is a class with a UI with loading progress bars
-# This script enables the preload of typically one scene but possibility of more scenes, being the last the scene to change to after the process.
+# This script enables the preload of typically one scene but possibility of more scenes
 # and also the precompilation of a series of materials of the scene to be load, the materials are loaded in this scene and also in the scene to be load
 # The way it works is :
 # On the ready function the first scene is loaded, the process function handles the process bar and when the scene is loaded and the bars are right positioned a signal is emitted after the scene_index is incremented
 # the signal is captured in the _launch_loading function that detects if there are more scenes to load or not
 # if there are still scenes the next scene is loaded and the process repeats
-# if not it is checked if thera are minimum one material, if not the last scene is shown
+# if not it is checked if thera are minimum one material, if the scene is shown
 # if there are materials, the previous process is done for the materials array items
 # When there are no more materials and the bar2 arrives 100% the last scene is shown
-@tool
+# It is also possible there are no scene to preload and only materials to precompile
 class_name LoadingScreen extends Node3D
 
 
-# Stores the scene to be loaded once the preload process has finished
-## Stores the scene to be loaded once the preload process has finished
+# Stores the scene to be loaded once the preload and precompile process has finished
+## Stores the scene to be loaded once the preload and precompile process has finished
 @export var _scene_path : String = ""
+
 
 # Stores the PackedScene to be loaded at the end of the process
 var _loading_scene : PackedScene = null
 # Node of the previous packedScene
 var _loading_scene_node : Node = null
 
-# Indicates if the scenes preloading process is runing
+
+
+
+
+# Indicates if the scenes preloading process is runing, used to say when all the scenes are preload
+# Used to load the scene to be shown previously the materials are precompiled
 var _scenesBeingLoaded : bool = false
+
+
 
 
 # All scenes to be loaded one after the other, can be empty
@@ -34,28 +42,44 @@ var _scenesBeingLoaded : bool = false
 @export var  _materials : Array[String] = []
 
 
+
+
+
 # Array so that the materials are not compiled twice
 var _foundMaterials: Array[Material] = []
+
+
+
+
 
 # Indicates which scene and which material of the previous arrays is being loaded
 var _scene_index : int = 0
 var _material_index : int = 0
 
 
+
 @export_range(5.0,250.0) var progress_speed : float = 100.0
 
 
-# Signel emitted when a scene is loaded and there are other scenes to be loaded
+
+
+# Signel emitted when a scene is loaded and there are other scenes to be loaded or materials to be compiled
 signal screenLoaded
+
+
 
 # Actual progress value; we move towards this value
 var _progress1_value : float = 0.0
 var _progress2_value : float = 0.0
 
+
+
 # variable to access the progress bar
 @onready var progress_bar1 : ProgressBar = $HUD/ProgressBar1
 @onready var progress_bar2 : ProgressBar = $HUD/ProgressBar2
 @onready var label2 : Label = $HUD/Label2
+
+
 
 
 func _notification(what):
@@ -67,10 +91,13 @@ func _notification(what):
 		queue_free()
 
 
+
+
 # Load the scene at the given path.
 # When this is finished loading, the "scene_loaded" signal will be emitted.
 func _ready() -> void:  
-	
+
+	# If in GameInstance are these parameters defined...
 	if GameInstance._scene_path != "" :
 		_scene_path = GameInstance._scene_path
 
@@ -80,6 +107,8 @@ func _ready() -> void:
 	if GameInstance._materials != null :
 		_materials = GameInstance._materials
 
+
+	# Progress Bar 2 should be used ?
 	# If the scenes array has only one member and there ara none material to be compiled the second progressbar has no sense, hiding it and putting to 100%
 	# If the scenes array is empty but there are materials to compile the second progressbar has no sense, hiding it and putting to 100%
 	if (_scene_paths.size() <= 1 and _materials.size() == 0) or (_scene_paths.size() == 0 and _materials.size() > 0):
@@ -91,23 +120,29 @@ func _ready() -> void:
 		_progress2_value = 0.0
 		progress_bar2.show()
 
+
+
 	# Connecting the signal to a function
 	screenLoaded.connect(_launch_loading)
+
+
 
 	# Initializing the progress bar 1 value
 	progress_bar1.value=0.0
 
 	# If there are no scenes go to the next step saying that the scenes loading process has begun
+	
 	if _scene_paths.size() == 0 :
 		_scenesBeingLoaded = true
 		screenLoaded.emit()
 	else : 
 		# Loading the first scene
-		label2.text = _scene_paths[0].split("/")[_scene_paths[0].split("/").size() - 1].split(".")[0]
+		_scene_index = 0
+		label2.text = _scene_paths[_scene_index].split("/")[_scene_paths[_scene_index].split("/").size() - 1].split(".")[0]
 
 		# Begin the scenes preloading process
 		_scenesBeingLoaded = true
-		_load_scene(_scene_paths[0])
+		_load_scene(_scene_paths[_scene_index])
 
 
 func _load_scene(path : String) :
@@ -136,6 +171,9 @@ func _load_material(path : String) :
 	#else :
 		# Begins the loading process...
 		ResourceLoader.load_threaded_request(path, "", true)
+
+
+
 
 
 # Function to process the signal emitted
@@ -222,8 +260,6 @@ func _process(delta: float):
 			if status == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_IN_PROGRESS :
 				_progress1_value = progress[0] * 100
 				progress_bar1.value = move_toward(progress_bar1.value, _progress1_value, delta * progress_speed)
-		
-			progress_bar2.value = move_toward(progress_bar2.value, _progress2_value, delta * progress_speed)
 
 			# Scene loading finished
 			if status == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_LOADED :
@@ -231,7 +267,7 @@ func _process(delta: float):
 				# put the progress bar to 100% so we don't get weird visuals and update the seconf bar progress
 				progress_bar1.value = move_toward(progress_bar1.value, 100.0, delta * progress_speed * _scene_paths.size())
 				
-				# Once the process has finished in the sense that the bar 1 arrives 100%
+				# Once the process has finished in the sense that the bar 1 arrives 100% the progress bar 2 moves to the corresponding value
 				if progress_bar1.value == 100 :
 					
 					# If there are no materials the bar 2 goes to 100%
@@ -241,16 +277,33 @@ func _process(delta: float):
 					else :
 						_progress2_value = (float) (_scene_index + 1) * 50 / _scene_paths.size()
 
+					# Only when the second bar arrives the corresponding value it goes to the next step
 					if round(progress_bar2.value) == round(_progress2_value) :
 						_scene_index += 1
 						screenLoaded.emit()
+
+				progress_bar2.value = move_toward(progress_bar2.value, _progress2_value, delta * progress_speed)
+	
 		else :
+
+			# Shaders compiling...
+			# Progress Bar 1 is used to show the process of material compilation
 			_progress1_value = _material_index as float * 100 / _materials.size() as float
+			
+			# The second bar only used if previous scenes where preloaded, in this case t goes from 50% to 100%
+			# If no scenes are being preloaded the second bar is not shown, just simply is set yo 100% but not used
 			if (_scene_paths.size() > 0) : _progress2_value = 50.0 + (_progress1_value / 2)
 			else : _progress2_value = 100
-			progress_bar1.value = move_toward(progress_bar1.value, _progress1_value, delta * progress_speed)
-			progress_bar2.value = move_toward(progress_bar2.value, _progress2_value, delta * progress_speed)
 
+			# The progress bar 1 in action...
+			progress_bar1.value = move_toward(progress_bar1.value, _progress1_value, delta * progress_speed)
+
+			# The progress bar 2 only in action if there were scenes preloaded
+			if (_scene_paths.size() > 0) :
+				progress_bar2.value = move_toward(progress_bar2.value, _progress2_value, delta * progress_speed)
+
+
+			# Order to precompile the material once is loaded and the progress_bar arrives its corresponding value
 			if _material_index < _materials.size() :
 
 				# If we have a valid material
@@ -258,19 +311,28 @@ func _process(delta: float):
 
 				# if is loaded and the progress bar has arrived the target controlled via the progress bar 1, it is used the round function to avoid difference in decimals
 				if status == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_LOADED and round(progress_bar1.value) == round(_progress1_value) :
+					
 					# The compilation is being done via the _precompile_material function
 					var mat : Material = ResourceLoader.load_threaded_get(_materials[_material_index]) as Material
 					_precompile_material(mat)
 
+					# Once compiled the material we continue with the next one
 					_material_index += 1
 					screenLoaded.emit()
+
 			# If we dont have any more material and the progress bar has arrived to 100%
 			# This is needed because we wait until bar2 arrives 100% once all materials are loaded
 			elif progress_bar1.value == 100 :
 				screenLoaded.emit()
-		# If we have none material, in reality no needed
+				
+	# If we have none material and the scene preload process has finished
+	# No needed because never arrives here due to in this case previously the scene is changed
 	else :
 		screenLoaded.emit()
+
+
+
+# Utility functions for precompiling the materials
 
 func _precompile_material(mat : Material) -> void :
 	# The compilation process just calling the _add_material function and manage the _foundmaterials array
